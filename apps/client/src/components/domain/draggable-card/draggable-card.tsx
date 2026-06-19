d2b3d720f10a3f95f73ffa2d67363257bdfe1efe
Rita Romenkova <rita.romenkova@gmail.com>
2026-06-20 00:43:36 +0300

    fix: drag and drop animation duration extended

--- apps/client/src/components/domain/draggable-card/draggable-card.tsx
+++ apps/client/src/components/domain/draggable-card/draggable-card.tsx
@@ -38,7 +38,7 @@ export function DraggableCard({ id, index, showBody, onDelete }: IProps) {
           {...provided.dragHandleProps}
           style={{
             ...provided.draggableProps.style,
-            ...(snapshot.isDropAnimating && { transitionDuration: "0.12s" }),
+            ...(snapshot.isDropAnimating && { transitionDuration: "0.5s" }),
           }}
           onClick={(e) => {
             if (snapshot.isDragging) return
